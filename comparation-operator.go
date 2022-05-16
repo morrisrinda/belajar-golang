877ede8f779249bc6109abf2e2d6f47dca413af8
morris <morrisrinda@gmail.com>
2022-05-16 23:25:57 +0700

    tambah belajar komparasi

--- comparation-operator.go
+++ comparation-operator.go
@@ -7,4 +7,13 @@ func main() {
 	var nama2 = "Morris"
 	var ceknama = nama1 == nama2
 	fmt.Println(ceknama)
+
+	var (
+		value1 = 100
+		value2 = 200
+	)
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
 }
